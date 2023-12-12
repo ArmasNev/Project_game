30856da9d56c466cc4d146aba6335b46c09e9c17
Patrik <patrik.hyytiainen@protonmail.com>
2023-12-12 13:18:11 +0200

    Added python script to get weather on current location

---
+++ weather.py
@@ -0,0 +1,36 @@
+import os
+import requests
+
+from dotenv import load_dotenv
+load_dotenv()
+
+
+class Weather:
+    def __init__(self, location):
+        apikey = os.environ.get("API_KEY")
+
+        if apikey is None:
+            raise ValueError("API_KEY environment variable is not set")
+
+        request = "https://api.openweathermap.org/data/2.5/weather?lat=" + \
+                  str(location["latitude"]) + "&lon=" + str(location["longitude"]) + "&appid=" + apikey
+
+        response = requests.get(request).json()
+
+        self.main = response["weather"][0]["main"]
+        self.weather_code = response["weather"][0]["id"]
+        self.description = response["weather"][0]["description"]
+        self.wind = {
+            "speed": response["wind"]["speed"]
+        }
+
+    def kelvin_to_celsius(self, kelvin):
+        return int(kelvin - 273.15)
+
+    def check_weather_condition(self):
+        if 500 <= int(self.weather_code) <= 622:
+            print(f"Correct weather condition: {self.main}")
+            return True
+        else:
+            print(f"Incorrect weather condition: {self.main}")
+            return False
