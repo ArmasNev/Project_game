069dc24eaa89b658a3f5c71d150ed25c45a3c450
ArmasNev <106472850+ArmasNev@users.noreply.github.com>
2023-12-16 20:46:36 +0200

    Update game.py

--- game.py
+++ game.py
@@ -1,30 +1,41 @@
 import random
 from database import Database
 from geopy import distance
-import config
-import json
+from geopy.distance import great_circle
 
 db = Database()
 
-class Game():
-    def __init__(self, id, loc, money, time, player=None):
-        self.location = loc
+class Game:
+    def __init__(self, game_id=None, name='Default'):
+        self.game_id = game_id
+        self.name = name
         self.airports = []
-        self.goals = []
-        self.money = money
-        self.time = time
-        self.player = player
-        self.id = id
+        self.current_location = None
+        self.money = 10000
+        self.time = 240
+        self.win = False
 
+        if self.game_id is None:
+            self.airports = self.get_airports()
+            if self.airports:
+                self.current_location = self.airports[0]['ident']
 
     def get_airports(self):
         sql = """SELECT iso_country, ident, name, type, latitude_deg, longitude_deg
-        FROM airport
-        WHERE type='large_airport'
-        ORDER by RAND()
-        LIMIT 21;"""                                  #?
+                 FROM airport
+                 WHERE type='large_airport'
+                 ORDER BY RAND()
+                 LIMIT 20;"""
         cursor = db.get_conn().cursor(dictionary=True)
         cursor.execute(sql)
+        return cursor.fetchall()
+
+    def get_airport_info(self, icao):
+        sql = f'''SELECT iso_country, ident, name, latitude_deg, longitude_deg
+                FROM airport
+                WHERE ident = %s'''
+        cursor = db.get_conn().cursor(dictionary=True)
+        cursor.execute(sql, (icao,))
         result = cursor.fetchall()
         return result
 
@@ -34,47 +45,6 @@ class Game():
         cursor.execute(sql)
         result = cursor.fetchall()
         return result
-
-
-    def new_game(self, a_ports):
-        sql = """INSERT INTO game (name, location, time, bank) VALUES (%s, %s, %s, %s);"""
-        cursor = db.get_conn().cursor(dictionary=True)
-        cursor.execute(sql, (self.player, self.location, self.time, self.money))
-        g_id = cursor.lastrowid
-
-        events = self.get_events()
-        event_id = {1: 0.4, 3: 0.4, 4: 0.2}
-        id_list = []
-
-        for i in range(0, 19):
-            random_id = random.choices(list(event_id.keys()),
-                                       list(event_id.values()))
-            id_list.append(random_id[0])
-        id_list.append(5)
-        random.shuffle(id_list)
-
-        e_ports = a_ports[1:].copy()
-        random.shuffle(e_ports)
-        response = e_ports
-
-
-
-        for i, event_id in enumerate(id_list):
-            sql = "INSERT INTO events (location, event_id, game_id) VALUES (%s, %s, %s);"
-            cursor = db.get_conn().cursor(dictionary=True)
-            cursor.execute(sql, (e_ports[i]['ident'], event_id, g_id))
-        return g_id, json.dumps(response)
-
-    # Get information about airport
-    def get_airport_info(self, icao):
-        sql=f'''SELECT iso_country, ident, name, latitude_deg, longitude_deg
-                FROM airport
-                WHERE ident = %s'''
-        cursor = db.get_conn().cursor(dictionary=True)
-        cursor.execute(sql, (icao,))
-        result = cursor.fetchall()
-        return result
-
 
     def airport_distance(self, current, target):
         start = self.get_airport_info(current)[0]  # Access the first (and only) item in the list
@@ -91,8 +61,6 @@ class Game():
                 in_range.append(a_port)
         return in_range
 
-
-
     def check_event(self, g_id, cur_airport):
         sql = '''
             SELECT events.id, event.id as event_id, event.min, event.max, events.game_id
@@ -108,122 +76,134 @@ class Game():
             return None
         return result
 
-
     def update_location(self, g_id, name, icao, m, time):
         sql = '''UPDATE game SET name = %s, location = %s,  bank = %s, time = %s  WHERE id = %s'''
         cursor = db.get_conn().cursor(dictionary=True)
         cursor.execute(sql, (name, icao, self.money, time, g_id))
 
+    def new_game(self):
+        if self.current_location is None and self.airports:
+            self.current_location = self.airports[0]['ident']
 
-"""
-    t_limit = 0
-    while True:
-        pet=input('What pet did you bring with you, a cat or a dog? ').lower()
-        if pet == "cat":
-            t_limit = 240
-            break
-        elif pet == "dog":
-            t_limit = 168
-            break
-        else:
-            print('Sorry, you can only take a cat or a dog.')
+        sql = """INSERT INTO game (name, location, bank, time) VALUES (%s, %s, %s, %s);"""
+        cursor = db.get_conn().cursor(dictionary=True)
+        cursor.execute(sql, (self.name, self.current_location, self.money, self.time))
+        self.game_id = cursor.lastrowid
+
+        pet_airport = random.choice(self.airports)
+        sql = "INSERT INTO events (location, event_id, game_id) VALUES (%s, 5, %s);"
+        cursor.execute(sql, (pet_airport['ident'], self.game_id))
+
+        remaining_airports = [airport for airport in self.airports if airport['ident'] != pet_airport['ident']]
+
+        event_id_weights = {1: 0.3, 3: 0.2, 4: 0.5}
+
+        for airport in remaining_airports:
+            event_id = random.choices(list(event_id_weights.keys()), weights=event_id_weights.values(), k=1)[0]
+            sql = "INSERT INTO events (location, event_id, game_id) VALUES (%s, %s, %s);"
+            cursor.execute(sql, (airport['ident'], event_id, self.game_id))
+
+        db.get_conn().commit()
+        return self.game_id
+
+    def load_game(self):
+        if self.game_id is None:
+            return False
+
+        sql = "SELECT * FROM game WHERE id = %s;"
+        cursor = db.get_conn().cursor(dictionary=True)
+        cursor.execute(sql, (self.game_id,))
+        result = cursor.fetchone()
+
+        if not result:
+            return False
+
+        self.current_location = result['location']
+        self.money = result['bank']
+        self.time = result['time']
+
+        return True
+
+    def fly_to(self, target_airport_id):
+        print(self.current_location, target_airport_id)
+        if self.current_location == target_airport_id:
+            raise Exception("You are already at this airport.")
 
 
-    # boolean for game over and win
-    game_over = False
-    win = False
-    money = 10000
-
-    score = 0
-    pet_found = False
-
-    # all airports
-    all_airports = get_airports()
-    # start_airport ident
-    s_airport = all_airports[0]['ident']
-
-    # current airport
-    current_airport = s_airport
-
-    game_id = new_game(player, s_airport, t_limit, money, all_airports)
-
-    game_over = False
+        current_airport = self.get_airport_info(self.current_location)
+        target_airport = self.get_airport_info(target_airport_id)
 
 
-    while not game_over:
-        event = check_event(game_id, current_airport)
-        airports = airports_in_range(current_airport, all_airports, p_range)
-        print(f'There are {len(airports)} airports in range: ')
-
-        # Check if player is out of range
-        if p_range < 0:
-            print('You are out of range.')
-            game_over = True
+        if not current_airport or not target_airport:
+            raise Exception("Airport information not found.")
 
 
-        # Get current airport info
-        airport = get_airport_info(current_airport)
+        try:
+            distance_km = great_circle((current_airport[0]['latitude_deg'], current_airport[0]['longitude_deg']),
+                                       (target_airport[0]['latitude_deg'], target_airport[0]['longitude_deg'])).kilometers
+        except Exception as e:
+            raise Exception(f"Error calculating distance: {str(e)}")
 
-        # Show game status
-        print(f"You are at {airport[0]['ident']} ({airport[0]['name']}).")
-        print(f'You have {money:.0f}$and {t_limit} hours left to find the {pet}.')
-        input(f"Press Enter to continue: ")
-        print('Your pet is in one of these airports: ')
+        cost = distance_km / 40
+        if self.money < cost:
+            raise Exception(f"Insufficient funds for this flight. Required: {cost}, Available: {self.money}")
 
-        sorted_airports = sorted(all_airports, key=lambda x: airport_distance(current_airport, x["ident"]))
 
-        # Print sorted airports
-        for i, airport in enumerate(sorted_airports):
-            ap_distance = airport_distance(current_airport, airport["ident"])
-            if ap_distance < p_range:
-                print(f'{i + 1}. {airport["name"]}, icao: {airport["ident"]}, distance: {ap_distance:.0f}km)')
-
-        # Ask for destination
-        print(f'You have money left for a {p_range} km flight.')
-        dest = input(f'Where do you want to go? ICAO: ')
-        selected_distance = airport_distance(current_airport, dest)
-        money -= selected_distance // 4
-        t_limit -= selected_distance // 1000
-        p_range = money * 4
-        update_location(game_id, player, dest, money, t_limit)
-        current_airport = dest
-
-        # Check if pet is found and player is at start (game won)
-        if win and current_airport == s_airport:
-            print(f'You are at {airport[0]["name"]}.')
-            print(f'You have {money:.0f}$ and {t_limit} hours left to find the {pet}.')
-            game_over = True
-
-        event = check_event(game_id, current_airport)
+        self.current_location = target_airport_id
+        self.money -= cost
+        self.time -= 30
+        self.update_game_state()
+        event = self.check_event(self.game_id, self.current_location)
+        event_message = ""
         if event:
-            event_id = event.get('event_id', None)
-            min_value = event.get('min', 0)
-            max_value = event.get('max', 0)
+            event_message = self.handle_event(event)
 
-            if event_id == 1:
-                temp_money = random.randrange(min_value, max_value, 100)
-                money -= temp_money
+        return event_message
 
-                t_limit = t_limit - 10
-                print(f"Customs check! You just lost {temp_money}$ and 10 hours!")
-            elif event_id == 2:
-                print(f"Storm! Your next flight is delayed.")
-                t_limit -= 20
-            elif event_id == 3:
-                temp_money = random.randrange(min_value, max_value, 100)
-                money += temp_money
-                print(f"Incoming money transfer received! You just got {temp_money}$!")
+    def handle_event(self, event):
+        event_id = event.get('event_id')
+        min_value = event.get('min', 0)
+        max_value = event.get('max', 0)
 
-            elif event_id == 4:
-                pass  # Do nothing for event 4
-            elif event_id == 5:
-                print(f"You found the {pet}!")
-                win = True
-            input(f"Press Enter to continue!")
+        if event_id == 1:
+            temp_money = random.randrange(min_value, max_value + 1, 100)
+            self.money -= temp_money
+            self.time -= 10
+            event_message = f"Customs check!{temp_money}$ and 10 hours lost!"
+            return event_message
 
-        if money <= 0:
-            print(f"You are out of money!")
-            game_over = True
+        elif event_id == 2:
+            pass
+
+        elif event_id == 3:
+            temp_money = random.randrange(min_value, max_value + 1, 100)
+            self.money += temp_money
+            event_message = f"Incoming money transfer received! {temp_money}$!"
+            return event_message
+
+        elif event_id == 4:
+            pass
+
+        elif event_id == 5:
+            self.win = True
+            event_message = "Congratulations! You found the pet and won the game!"
+            print(event_message)
+            return event_message
+
+    def is_game_over(self):
+        return self.money <= 0 or self.time <= 0
+
+    def game_over_status(self):
+        if self.money <= 1000:
+            return "Game Over: You have not enough money to flight!"
+        elif self.time <= 0:
+            return "Game Over: You are out of time!"
+        else:
+            return "Game continues"
+    def update_game_state(self):
+        sql = '''UPDATE game SET location = %s, bank = %s, time = %s WHERE id = %s'''
+        cursor = db.get_conn().cursor(dictionary=True)
+        cursor.execute(sql, (self.current_location, self.money, self.time, self.game_id))
+        db.get_conn().commit()
 
 
-"""
